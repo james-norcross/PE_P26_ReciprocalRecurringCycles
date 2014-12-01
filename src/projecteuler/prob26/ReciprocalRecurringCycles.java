e61598ccbfdd6d24d978d1c79f8b19c5cd6786e1
james-norcross <james.norcross@gmail.com>
2014-12-01 14:52:25 -0600

    initial commit

---
+++ src/projecteuler/prob26/ReciprocalRecurringCycles.java
@@ -0,0 +1,16 @@
+package projecteuler.prob26;
+
+public class ReciprocalRecurringCycles {
+
+	/**
+	 * Author: James Norcross
+	 * Date: 12/1/14
+	 * Purpose: Project Euler Problem 26 solution
+	 * Description: finds the integer d <1000 for which 1/d has the longest recurring cycle
+	 */
+	
+	public static void main(String[] args) {
+
+	}
+
+}
